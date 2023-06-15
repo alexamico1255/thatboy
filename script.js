bbe9ae33ac17aadb63de2a86f87c0651a05bce55
alexamico1255 <97551181+alexamico1255@users.noreply.github.com>
2023-06-15 18:27:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var depopLogo = document.querySelector(".social-media-cell img");
+    depopLogo.addEventListener("click", function() {
+      window.location.href = "https://www.instagram.com/thatboythrift/";
+    });
+  });
